2e9ac325373f0a6eccefda9216b574bac519736e
1624071973 <1624071973@qq.com>
2019-03-31 23:32:23 +0800

    000
    Signed-off-by: 1624071973 <1624071973@qq.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+//引入当前组件
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //定义当前组件名称
+  templateUrl: './app.component.html',//组件界面
+  styleUrls: ['./app.component.css']//组件css
 })
 export class AppComponent {
-  title = 'angulardemo';
+  title = 'angulardemo';//属性
+
+  constructor() { //构造函数
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+//**Anuglar根模块 */ 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DataMarketComponent } from './components/data-market/data-market.component';
-import { HearComponent } from './components/hear/hear.component';
-import { MainComponent } from './components/main/main.component';
+//BrowserModule，浏览器解析模块
+import {BrowserModule} from '@angular/platform-browser';
+//angular核心模块
+import {NgModule} from '@angular/core';
+//路由模块
+import {AppRoutingModule} from './app-routing.module';
+//根组件
+import {AppComponent} from './app.component';
+//注册自定义组件
+import {DataMarketComponent} from './components/data-market/data-market.component';
+import {HearComponent} from './components/hear/hear.component';
+import {MainComponent} from './components/main/main.component';
 
+//NgModule装饰器，接受一个元数据对象，告诉angular如何编译和启动
 @NgModule({
-  declarations: [
+  declarations: [//配置当前项目运行的组件
     AppComponent,
     DataMarketComponent,
     HearComponent,
     MainComponent
   ],
-  imports: [
+  imports: [ //配置当前模块运行依赖的其他模块
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//配置项目所需的服务
+  bootstrap: [AppComponent]//指定应用的主视图，通过引导模块引导AppMoudle启动应用，通常是根组件
 })
-export class AppModule { }
+//暴露当前模块
+export class AppModule {
+}
